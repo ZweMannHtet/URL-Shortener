df2348047b1328ce2569169d6196bd7d71374543
ZweMannHtet <zweman98240@gmail.com>
2023-10-21 17:08:15 +0630

    Add color for page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,21 @@ module.exports = {
       colors:{
         'pale-blue':'#20B2AA',
         'gray': 'c4c4c4',
-        'pale':'#ddd'
+        'pale':'#ddd',
+        'pale-color': '#ddd',
+        'li-back':'#f2f2f2',
+        'info-color': '#0f5753',
+        'info-back': '#bef4f1',
+        'info-border': '#7de8e3'
       },
       width:{
-        'custom': '50rem'
+        'custom': '50rem',
+        'popup-width': '30em'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar')
+  ],
 }
 
